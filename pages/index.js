dc3be80273feb6e89d61d9ade38c0a182b9851ed
Jeehoon Sim <jinsim3201@gmail.com>
2022-08-08 20:03:49 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Page = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+export default Page
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 2,
+    bracketSpacing: true,
+    semi: false
+}
+module.exports = options 
