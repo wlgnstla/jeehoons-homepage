07a1b8dfa8f9123ada0e725a3745e4010f3d38f7
Jin <sjeehoon@umich.edu>
2024-08-01 15:11:55 -0400

    refined website logo

--- components/logo.js
+++ components/logo.js
@@ -1,43 +1,48 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import {Text, useColorModeValue} from '@chakra-ui/react'
-import styled from '@emotion/styled'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Text, useColorModeValue } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 const LogoBox = styled.span`
-font-weight:bold;
-font-size: 18px;
-display: inline-flex;
-align-items:center;
-height:20px;
-line-height:20px;
-padding:10px;
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 60px;
+  line-height: 20px;
+  padding: 10px;
+  transition: transform 03s ease-in-out;
 
-&:hover img {
-    transform: rotate(10deg)
-}
-`
+  img {
+    transition: transform 0.3s ease-in-out;
+  }
+
+  &:hover img {
+    transform: rotate(10deg);
+  }
+`;
 
 const Logo = () => {
-    
-    const narutoImg = `/images/naruto${useColorModeValue('','-dark')}.png`
-    return (
-        <Link href='/'>
-            <a>
-            <LogoBox>
-                <Image src={narutoImg} width='20px' height='20px' alt='logo'/> 
-            <Text
-                color={useColorModeValue('gray.800','whiteAlpha.900')} 
-                fontFamily='M PLUS Rounded 1c'
-                fontWeight='bold'
-                ml={3}
-            >
-                Jeehoon Sim
-            </Text>
-            </LogoBox>
-            </a>
-        </Link>
-    )
-}
+  const narutoImg = `/images/naruto${useColorModeValue('-darks', '')}.svg`;
 
+  return (
+    <Link href='/'>
+      <a>
+        <LogoBox>
+          <Image src={narutoImg} width={20} height={20} alt='logo' />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily='M PLUS Rounded 1c'
+            fontWeight='bold'
+            ml={3}
+          >
+            Jeehoon Sim
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;
+
